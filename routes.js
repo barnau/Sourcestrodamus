d412fc68643b58d8733af75a4e2ff350e8058d45
Blaine Arnau <blainearnau@gmail.com>
2016-11-02 12:01:44 -0700

    Add routes for answers

--- routes.js
+++ routes.js
@@ -22,11 +22,53 @@ router.post("/", function(req ,res) {
 //GET /questions/:id
 //Route for specific questions
 
-router.get("/:id", function(req,res) {
+router.get("/:qID", function(req,res) {
 	res.json({
-		response: "You sent me a get request for ID: " + req.params.id
+		response: "You sent me a get request for ID: " + req.params.qID
 	})
 });
 
+//POST /questions/:id/answers
+//Route for creating an answer
+router.post("/:qID/answers", function(req ,res) {
+	res.json({
+		response: "You sent me a POST request to /answers",
+		questionId: req.params.qID,
+		body: req.body
+	})
+});
+
+//PUT /questions/:id/answers/:id
+//Edit a specific answer
+router.put("/:qID/answers/:aID", function(req, res) {
+	res.json({
+		response: "You sent me a put request for answers: ",
+		questionId: req.params.qID,
+		answerId: req.params.aID,
+		body: req.body
+	});
+})
+
+//DELETE /questions/:id/answers/:id
+//Delete a specific answer
+router.delete("/:qID/answers/:aID", function(req, res) {
+	res.json({
+		response: "You sent me a DELTE request to /answers ",
+		questionId: req.params.qID,
+		answerId: req.params.aID
+	});
+})
+
+//POST /questions/:id/answers/:id/vote-up
+//POST /questions/:id/answers/:id/vote-down
+//Vote on a specific answer
+router.post("/:qID/answers/:aID/vote-:dir", function(req, res) {
+	res.json({
+		response: "You sent me a POST request to /vote-" + req.params.dir,
+		questionId: req.params.qID,
+		answerId: req.params.aID,
+		vote: req.params.dir
+	});
+})
 
 module.exports = router;
